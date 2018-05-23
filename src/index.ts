4264401a56e21381aa4f306ab9e08b8218682ca3
James Monger <jkmonger96@gmail.com>
2018-05-23 11:01:55 +0100

    Download library document

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import getArgs from "./get-args";
+import * as fs from "fs";
+const AdobeSignSdk = require("adobe-sign-sdk");
 
 const args = process.argv.slice(2);
 const { documentType, accessToken, documentId } = getArgs(args);
 
-console.log(documentType, accessToken, documentId);
+const context = new AdobeSignSdk.Context();
+const libraryDocuments = new AdobeSignSdk.LibraryDocumentsApi(context);
+
+libraryDocuments.getCombinedDocument({ accessToken }, documentId)
+    .then(a => {
+        console.log("about to write");
+        fs.writeFile(`${documentId}.pdf`, a, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+          });
+    });
