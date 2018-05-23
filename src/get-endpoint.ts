556567b6747c861455206ec17d1d20347f04f8bc
James Monger <jkmonger96@gmail.com>
2018-05-23 14:57:04 +0100

    Fix line length

--- src/get-endpoint.ts
+++ src/get-endpoint.ts
@@ -7,7 +7,10 @@ export default (documentType: DocumentType) => {
     const context = new AdobeSignSdk.Context();
 
     // we can assume that it's either library document or agreement as that's checked elsewhere
-    const api = (documentType === "libraryDocument") ? new AdobeSignSdk.LibraryDocumentsApi(context) : new AdobeSignSdk.AgreementsApi(context);
+    const api =
+        (documentType === "libraryDocument")
+        ? new AdobeSignSdk.LibraryDocumentsApi(context)
+        : new AdobeSignSdk.AgreementsApi(context);
 
     return api.getCombinedDocument;
 };
